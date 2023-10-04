wipwd/s3gw-ceph

rgw/sfs: Initial Garbage collector thread.
This adds an initial GC approach just starting a thread and running
 a basic remove deleted buckets operation.

The implementation of how the thread is started and stopped are taken
from rgw's code for consistency across stores.

It uses 2 parameters from rgw:

- `rgw_gc_processor_period` : defines the time GC waits to be triggered
- `rgw_gc_max_objs` : defines the maximum number of items the GC can
  remove per iteration.

Any possible actions done to the `sqlite` metadata is considered an item
(object) when taking into account the maxium number of items to be
processed in a single iteration as those actions block the metadata
database.

Fixes: https://github.com/aquarist-labs/s3gw/issues/165
Signed-off-by: Xavi Garcia <xavi.garcia@suse.com>

src/rgw/rgw_sal_sfs.cc
@@ -43,6 +43,7 @@
 #include "services/svc_zone_utils.h"
 
 #include "store/sfs/notification.h"
+#include "store/sfs/sfs_gc.h"
 #include "store/sfs/writer.h"
 
 #include "rgw/store/sfs/sqlite/sqlite_users.h"
@@ -382,11 +383,16 @@ int SFStore::initialize(
   const DoutPrefixProvider* dpp
 ) {
   ldpp_dout(dpp, 10) << __func__ << dendl;
+  gc = new sfs::SFSGC();
+  gc->initialize(cct, this);
+  gc->start_processor();
   return 0;
 }
 
 void SFStore::finalize(void) {
   ldout(ctx(), 10) << __func__ << ": TODO" << dendl;
+  delete gc;
+  gc = nullptr;
   return;
 }
 

0xavi0/ceph

src/rgw/rgw_sal_sfs.cc
@@ -25,6 +25,7 @@
 #include "common/Clock.h"
 #include "common/errno.h"
 #include "driver/sfs/notification.h"
+#include "driver/sfs/sfs_gc.h"
 #include "driver/sfs/writer.h"
 #include "rgw/driver/sfs/sqlite/sqlite_buckets.h"
 #include "rgw/driver/sfs/sqlite/sqlite_users.h"
@@ -384,11 +385,16 @@ int SFStore::log_op(
 
 int SFStore::initialize(CephContext* cct, const DoutPrefixProvider* dpp) {
   ldpp_dout(dpp, 10) << __func__ << dendl;
+  gc = new sfs::SFSGC();
+  gc->initialize(cct, this);
+  gc->start_processor();
   return 0;
 }
 
 void SFStore::finalize(void) {
   ldout(ctx(), 10) << __func__ << ": TODO" << dendl;
+  delete gc;
+  gc = nullptr;
   return;
 }
 