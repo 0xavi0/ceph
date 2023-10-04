wipwd/s3gw-ceph

rgw/sfs: rename simplefile to sfs
Signed-off-by: Joao Eduardo Luis <joao@suse.com>

src/rgw/store/sfs/multipart.h
@@ -2,7 +2,7 @@
 // vim: ts=8 sw=2 smarttab ft=cpp
 /*
  * Ceph - scalable distributed file system
- * Simple filesystem SAL implementation
+ * SFS SAL implementation
  *
  * Copyright (C) 2022 SUSE LLC
  *
@@ -11,16 +11,16 @@
  * License version 2.1, as published by the Free Software
  * Foundation. See file COPYING.
  */
-#ifndef RGW_STORE_SIMPLEFILE_MULTIPART_H
-#define RGW_STORE_SIMPLEFILE_MULTIPART_H
+#ifndef RGW_STORE_SFS_MULTIPART_H
+#define RGW_STORE_SFS_MULTIPART_H
 
 #include "rgw_sal.h"
 
 namespace rgw::sal {
 
-class SimpleFileStore;
+class SFStore;
 
-class SimpleFileMultipartMeta {
+class SFSMultipartMeta {
 
   ACLOwner owner;
   Attrs attrs;
@@ -30,7 +30,7 @@ class SimpleFileMultipartMeta {
   ceph::real_time mtime;
 
  public:
-  SimpleFileMultipartMeta(
+  SFSMultipartMeta(
     ACLOwner &_owner,
     Attrs &_attrs,
     rgw_placement_rule &_dest_placement,
@@ -61,9 +61,9 @@ class SimpleFileMultipartMeta {
   }
 
 };
-WRITE_CLASS_ENCODER(SimpleFileMultipartMeta)
+WRITE_CLASS_ENCODER(SFSMultipartMeta)
 
-class SimpleFileMultipartObject {
+class SFSMultipartObject {
 
   std::string oid;  // object id / name
   std::string upload_id;
@@ -73,12 +73,12 @@ class SimpleFileMultipartObject {
   void init(CephContext *cct, std::string _oid, std::string _upload_id);
 
  public:
-  SimpleFileMultipartObject(
+  SFSMultipartObject(
     CephContext *cct,
     const std::string &_oid,
     const std::string &_upload_id
   );
-  SimpleFileMultipartObject(
+  SFSMultipartObject(
     CephContext *cct,
     const std::string &_oid,
     const std::optional<std::string> _upload_id
@@ -97,7 +97,7 @@ class SimpleFileMultipartObject {
   }
 
   friend inline std::ostream& operator<<(
-    std::ostream &out, const SimpleFileMultipartObject &obj
+    std::ostream &out, const SFSMultipartObject &obj
   ) {
     out << "multipart_object(oid: " << obj.get_key() << ", upload_id: "
         << obj.get_upload_id() << ", meta: " << obj.get_meta() << ")";
@@ -106,19 +106,19 @@ class SimpleFileMultipartObject {
 
 };
 
-class SimpleFileMultipartUpload : public MultipartUpload {
+class SFSMultipartUpload : public MultipartUpload {
 
-  SimpleFileStore *store;
+  SFStore *store;
   const std::string &oid;
   std::optional<std::string> upload_id;
-  SimpleFileMultipartObject obj;
+  SFSMultipartObject obj;
   ACLOwner owner;
   ceph::real_time mtime;
 
  public:
-  SimpleFileMultipartUpload(
+  SFSMultipartUpload(
     CephContext *_cct,
-    SimpleFileStore *_store,
+    SFStore *_store,
     Bucket *_bucket,
     const std::string &_oid,
     std::optional<std::string> _upload_id,
@@ -129,7 +129,7 @@ class SimpleFileMultipartUpload : public MultipartUpload {
       obj(_cct, _oid, _upload_id),
       owner(_owner), mtime(_mtime) { }
 
-  virtual ~SimpleFileMultipartUpload() = default;
+  virtual ~SFSMultipartUpload() = default;
 
   virtual const std::string& get_meta() const {
     return obj.get_meta();
@@ -194,11 +194,11 @@ class SimpleFileMultipartUpload : public MultipartUpload {
   void dump(Formatter *f) const;
   int write_metadata(
     const DoutPrefixProvider *dpp,
-    SimpleFileMultipartMeta &metadata
+    SFSMultipartMeta &metadata
   );
   inline std::string get_cls_name() { return "multipart_upload"; }
 };
 
 } // ns rgw::sal
 
-#endif // RGW_STORE_SIMPLEFILE_MULTIPART_H
+#endif // RGW_STORE_SFS_MULTIPART_H

src/rgw/store/sfs/notification.h
@@ -2,7 +2,7 @@
 // vim: ts=8 sw=2 smarttab ft=cpp
 /*
  * Ceph - scalable distributed file system
- * Simple filesystem SAL implementation
+ * SFS SAL implementation
  *
  * Copyright (C) 2022 SUSE LLC
  *
@@ -11,24 +11,24 @@
  * License version 2.1, as published by the Free Software
  * Foundation. See file COPYING.
  */
-#ifndef RGW_STORE_SIMPLEFILE_NOTIFICATION_H
-#define RGW_STORE_SIMPLEFILE_NOTIFICATION_H
+#ifndef RGW_STORE_SFS_NOTIFICATION_H
+#define RGW_STORE_SFS_NOTIFICATION_H
 
 #include "rgw_sal.h"
 
 namespace rgw::sal {
 
-class SimpleFileNotification : public Notification {
+class SFSNotification : public Notification {
 
 public:
 
-  SimpleFileNotification(
+  SFSNotification(
     Object *obj,
     Object *src_obj,
     rgw::notify::EventType type
   ) : Notification(obj, src_obj, type) { }
 
-  ~SimpleFileNotification() = default;
+  ~SFSNotification() = default;
 
   virtual int publish_reserve(
     const DoutPrefixProvider *dpp,
@@ -51,4 +51,4 @@ public:
 
 } // ns rgw::sal
 
-#endif // RGW_STORE_SIMPLEFILE_NOTIFICATION_H
+#endif // RGW_STORE_SFS_NOTIFICATION_H

src/rgw/store/sfs/writer.h
@@ -2,7 +2,7 @@
 // vim: ts=8 sw=2 smarttab ft=cpp
 /*
  * Ceph - scalable distributed file system
- * Simple filesystem SAL implementation
+ * SFS SAL implementation
  *
  * Copyright (C) 2022 SUSE LLC
  *
@@ -11,22 +11,22 @@
  * License version 2.1, as published by the Free Software
  * Foundation. See file COPYING.
  */
-#ifndef RGW_STORE_SIMPLEFILE_WRITER_H
-#define RGW_STORE_SIMPLEFILE_WRITER_H
+#ifndef RGW_STORE_SFS_WRITER_H
+#define RGW_STORE_SFS_WRITER_H
 
 #include <memory>
 #include "rgw_sal.h"
-#include "store/simplefile/bucket_mgr.h"
+#include "store/sfs/bucket_mgr.h"
 
 namespace rgw::sal {
 
-class SimpleFileStore;
+class SFStore;
 
-class SimpleFileAtomicWriter : public Writer {
+class SFSAtomicWriter : public Writer {
 
  protected:
-  rgw::sal::SimpleFileStore *store;
-  SimpleFileObject obj;
+  rgw::sal::SFStore *store;
+  SFSObject obj;
   BucketMgrRef mgr;
   const rgw_user &owner;
   const rgw_placement_rule *placement_rule;
@@ -35,18 +35,18 @@ class SimpleFileAtomicWriter : public Writer {
   uint64_t bytes_written;
 
  public:
-  SimpleFileAtomicWriter(
+  SFSAtomicWriter(
     const DoutPrefixProvider *_dpp,
     optional_yield _y,
     std::unique_ptr<rgw::sal::Object> _head_obj,
-    SimpleFileStore *_store,
+    SFStore *_store,
     BucketMgrRef _mgr,
     const rgw_user& _owner,
     const rgw_placement_rule *_ptail_placement_rule,
     uint64_t _olh_epoch,
     const std::string &_unique_tag
   );
-  ~SimpleFileAtomicWriter() = default;
+  ~SFSAtomicWriter() = default;
 
   virtual int prepare(optional_yield y) override;
   virtual int process(bufferlist &&data, uint64_t offset) override;
@@ -69,24 +69,24 @@ class SimpleFileAtomicWriter : public Writer {
 
 };
 
-class SimpleFileMultipartWriter : public Writer {
+class SFSMultipartWriter : public Writer {
 
  protected:
-  const rgw::sal::SimpleFileStore *store;
+  const rgw::sal::SFStore *store;
 
  public:
-  SimpleFileMultipartWriter(
+  SFSMultipartWriter(
     const DoutPrefixProvider *_dpp,
     optional_yield y,
     MultipartUpload *upload,
     std::unique_ptr<rgw::sal::Object> _head_obj,
-    const SimpleFileStore *store,
+    const SFStore *store,
     const rgw_user &_owner,
     const rgw_placement_rule *_ptail_placement_rule,
     uint64_t _part_num,
     const std::string &_part_num_str
   );
-  ~SimpleFileMultipartWriter() = default;
+  ~SFSMultipartWriter() = default;
 
   virtual int prepare(optional_yield y) override;
   virtual int process(bufferlist &&data, uint64_t offset) override;
@@ -109,4 +109,4 @@ class SimpleFileMultipartWriter : public Writer {
 
 } // ns rgw::sal
 
-#endif // RGW_STORE_SIMPLEFILE_WRITER_H
+#endif // RGW_STORE_SFS_WRITER_H

0xavi0/ceph

src/rgw/driver/sfs/multipart.h
@@ -2,7 +2,7 @@
 // vim: ts=8 sw=2 smarttab ft=cpp
 /*
  * Ceph - scalable distributed file system
- * Simple filesystem SAL implementation
+ * SFS SAL implementation
  *
  * Copyright (C) 2022 SUSE LLC
  *
@@ -11,17 +11,17 @@
  * License version 2.1, as published by the Free Software
  * Foundation. See file COPYING.
  */
-#ifndef RGW_STORE_SIMPLEFILE_MULTIPART_H
-#define RGW_STORE_SIMPLEFILE_MULTIPART_H
+#ifndef RGW_STORE_SFS_MULTIPART_H
+#define RGW_STORE_SFS_MULTIPART_H
 
 #include "rgw_sal.h"
 #include "rgw_sal_store.h"
 
 namespace rgw::sal {
 
-class SimpleFileStore;
+class SFStore;
 
-class SimpleFileMultipartMeta {
+class SFSMultipartMeta {
   ACLOwner owner;
   Attrs attrs;
   rgw_placement_rule dest_placement;
@@ -30,7 +30,7 @@ class SimpleFileMultipartMeta {
   ceph::real_time mtime;
 
  public:
-  SimpleFileMultipartMeta(
+  SFSMultipartMeta(
       ACLOwner& _owner, Attrs& _attrs, rgw_placement_rule& _dest_placement,
       RGWObjCategory _category, int _flags, ceph::real_time& _mtime
   )
@@ -61,9 +61,9 @@ class SimpleFileMultipartMeta {
     DECODE_FINISH(bl);
   }
 };
-WRITE_CLASS_ENCODER(SimpleFileMultipartMeta)
+WRITE_CLASS_ENCODER(SFSMultipartMeta)
 
-class SimpleFileMultipartObject {
+class SFSMultipartObject {
   std::string oid;  // object id / name
   std::string upload_id;
   std::string meta;
@@ -72,10 +72,10 @@ class SimpleFileMultipartObject {
   void init(CephContext* cct, std::string _oid, std::string _upload_id);
 
  public:
-  SimpleFileMultipartObject(
+  SFSMultipartObject(
       CephContext* cct, const std::string& _oid, const std::string& _upload_id
   );
-  SimpleFileMultipartObject(
+  SFSMultipartObject(
       CephContext* cct, const std::string& _oid,
       const std::optional<std::string> _upload_id
   );
@@ -87,7 +87,7 @@ class SimpleFileMultipartObject {
   const std::string& get_meta() const { return meta; }
 
   friend inline std::ostream& operator<<(
-      std::ostream& out, const SimpleFileMultipartObject& obj
+      std::ostream& out, const SFSMultipartObject& obj
   ) {
     out << "multipart_object(oid: " << obj.get_key()
         << ", upload_id: " << obj.get_upload_id()
@@ -96,17 +96,17 @@ class SimpleFileMultipartObject {
   }
 };
 
-class SimpleFileMultipartUpload : public StoreMultipartUpload {
-  SimpleFileStore* store;
+class SFSMultipartUpload : public StoreMultipartUpload {
+  SFStore* store;
   const std::string& oid;
   std::optional<std::string> upload_id;
-  SimpleFileMultipartObject obj;
+  SFSMultipartObject obj;
   ACLOwner owner;
   ceph::real_time mtime;
 
  public:
-  SimpleFileMultipartUpload(
-      CephContext* _cct, SimpleFileStore* _store, Bucket* _bucket,
+  SFSMultipartUpload(
+      CephContext* _cct, SFStore* _store, Bucket* _bucket,
       const std::string& _oid, std::optional<std::string> _upload_id,
       ACLOwner _owner, ceph::real_time _mtime
   )
@@ -118,7 +118,7 @@ class SimpleFileMultipartUpload : public StoreMultipartUpload {
         owner(_owner),
         mtime(_mtime) {}
 
-  virtual ~SimpleFileMultipartUpload() = default;
+  virtual ~SFSMultipartUpload() = default;
 
   virtual const std::string& get_meta() const { return obj.get_meta(); }
   virtual const std::string& get_key() const { return obj.get_key(); }
@@ -158,12 +158,10 @@ class SimpleFileMultipartUpload : public StoreMultipartUpload {
   ) override;
 
   void dump(Formatter* f) const;
-  int write_metadata(
-      const DoutPrefixProvider* dpp, SimpleFileMultipartMeta& metadata
-  );
+  int write_metadata(const DoutPrefixProvider* dpp, SFSMultipartMeta& metadata);
   inline std::string get_cls_name() { return "multipart_upload"; }
 };
 
 }  // namespace rgw::sal
 
-#endif  // RGW_STORE_SIMPLEFILE_MULTIPART_H
+#endif  // RGW_STORE_SFS_MULTIPART_H

src/rgw/driver/sfs/notification.h
@@ -2,7 +2,7 @@
 // vim: ts=8 sw=2 smarttab ft=cpp
 /*
  * Ceph - scalable distributed file system
- * Simple filesystem SAL implementation
+ * SFS SAL implementation
  *
  * Copyright (C) 2022 SUSE LLC
  *
@@ -11,22 +11,20 @@
  * License version 2.1, as published by the Free Software
  * Foundation. See file COPYING.
  */
-#ifndef RGW_STORE_SIMPLEFILE_NOTIFICATION_H
-#define RGW_STORE_SIMPLEFILE_NOTIFICATION_H
+#ifndef RGW_STORE_SFS_NOTIFICATION_H
+#define RGW_STORE_SFS_NOTIFICATION_H
 
 #include "rgw_sal.h"
 #include "rgw_sal_store.h"
 
 namespace rgw::sal {
 
-class SimpleFileNotification : public StoreNotification {
+class SFSNotification : public StoreNotification {
  public:
-  SimpleFileNotification(
-      Object* obj, Object* src_obj, rgw::notify::EventType type
-  )
+  SFSNotification(Object* obj, Object* src_obj, rgw::notify::EventType type)
       : StoreNotification(obj, src_obj, type) {}
 
-  ~SimpleFileNotification() = default;
+  ~SFSNotification() = default;
 
   virtual int publish_reserve(
       const DoutPrefixProvider* dpp, RGWObjTags* obj_tags = nullptr
@@ -45,4 +43,4 @@ class SimpleFileNotification : public StoreNotification {
 
 }  // namespace rgw::sal
 
-#endif  // RGW_STORE_SIMPLEFILE_NOTIFICATION_H
+#endif  // RGW_STORE_SFS_NOTIFICATION_H

src/rgw/driver/sfs/writer.h
@@ -2,7 +2,7 @@
 // vim: ts=8 sw=2 smarttab ft=cpp
 /*
  * Ceph - scalable distributed file system
- * Simple filesystem SAL implementation
+ * SFS SAL implementation
  *
  * Copyright (C) 2022 SUSE LLC
  *
@@ -11,24 +11,24 @@
  * License version 2.1, as published by the Free Software
  * Foundation. See file COPYING.
  */
-#ifndef RGW_STORE_SIMPLEFILE_WRITER_H
-#define RGW_STORE_SIMPLEFILE_WRITER_H
+#ifndef RGW_STORE_SFS_WRITER_H
+#define RGW_STORE_SFS_WRITER_H
 
 #include <memory>
 
-#include "driver/simplefile/object.h"
-#include "driver/simplefile/bucket_mgr.h"
+#include "driver/sfs/object.h"
+#include "driver/sfs/bucket_mgr.h"
 #include "rgw_sal.h"
 #include "rgw_sal_store.h"
 
 namespace rgw::sal {
 
-class SimpleFileStore;
+class SFStore;
 
-class SimpleFileAtomicWriter : public StoreWriter {
+class SFSAtomicWriter : public StoreWriter {
  protected:
-  rgw::sal::SimpleFileStore* store;
-  SimpleFileObject obj;
+  rgw::sal::SFStore* store;
+  SFSObject obj;
   BucketMgrRef mgr;
   const rgw_user& owner;
   const rgw_placement_rule* placement_rule;
@@ -37,13 +37,13 @@ class SimpleFileAtomicWriter : public StoreWriter {
   uint64_t bytes_written;
 
  public:
-  SimpleFileAtomicWriter(
+  SFSAtomicWriter(
       const DoutPrefixProvider* _dpp, optional_yield _y,
-      rgw::sal::Object* _head_obj, SimpleFileStore* _store, BucketMgrRef _mgr,
+      rgw::sal::Object* _head_obj, SFStore* _store, BucketMgrRef _mgr,
       const rgw_user& _owner, const rgw_placement_rule* _ptail_placement_rule,
       uint64_t _olh_epoch, const std::string& _unique_tag
   );
-  ~SimpleFileAtomicWriter() = default;
+  ~SFSAtomicWriter() = default;
 
   virtual int prepare(optional_yield y) override;
   virtual int process(bufferlist&& data, uint64_t offset) override;
@@ -58,18 +58,18 @@ class SimpleFileAtomicWriter : public StoreWriter {
   const std::string get_cls_name() const { return "atomic_writer"; }
 };
 
-class SimpleFileMultipartWriter : public StoreWriter {
+class SFSMultipartWriter : public StoreWriter {
  protected:
-  const rgw::sal::SimpleFileStore* store;
+  const rgw::sal::SFStore* store;
 
  public:
-  SimpleFileMultipartWriter(
+  SFSMultipartWriter(
       const DoutPrefixProvider* _dpp, optional_yield y, MultipartUpload* upload,
-      rgw::sal::Object* _head_obj, const SimpleFileStore* store,
-      const rgw_user& _owner, const rgw_placement_rule* _ptail_placement_rule,
-      uint64_t _part_num, const std::string& _part_num_str
+      rgw::sal::Object* _head_obj, const SFStore* store, const rgw_user& _owner,
+      const rgw_placement_rule* _ptail_placement_rule, uint64_t _part_num,
+      const std::string& _part_num_str
   );
-  ~SimpleFileMultipartWriter() = default;
+  ~SFSMultipartWriter() = default;
 
   virtual int prepare(optional_yield y) override;
   virtual int process(bufferlist&& data, uint64_t offset) override;
@@ -84,4 +84,4 @@ class SimpleFileMultipartWriter : public StoreWriter {
 
 }  // namespace rgw::sal
 
-#endif  // RGW_STORE_SIMPLEFILE_WRITER_H
+#endif  // RGW_STORE_SFS_WRITER_H