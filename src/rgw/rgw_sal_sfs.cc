wipwd/s3gw-ceph

rgw/sfs: Add basic user management via rest api.
This adds basic user management via rest api

Adds also a very basic versioning, increasing the version everytime the
user info is stored and checks for version when loading the user.

Added a few unittests for testing higher lever user management.

Signed-off-by: Xavi Garcia <xavi.garcia@suse.com>

src/rgw/rgw_sal_sfs.cc
@@ -46,6 +46,8 @@
 #include "store/sfs/notification.h"
 #include "store/sfs/writer.h"
 
+#include "rgw/store/sfs/sqlite/sqlite_users.h"
+
 #define dout_subsys ceph_subsys_rgw
 
 using namespace std;
@@ -144,8 +146,7 @@ int SFStore::forward_request_to_master(const DoutPrefixProvider *dpp,
                                                bufferlist &in_data,
                                                JSONParser *jp, req_info &info,
                                                optional_yield y) {
-  ldpp_dout(dpp, 10) << __func__ << ": TODO" << dendl;
-  return -ENOTSUP;
+  return 0;
 }
 
 int SFStore::forward_iam_request_to_master(
@@ -254,7 +255,10 @@ int SFStore::meta_list_keys_init(const DoutPrefixProvider *dpp,
 int SFStore::meta_list_keys_next(const DoutPrefixProvider *dpp,
                                          void *handle, int max,
                                          list<string> &keys, bool *truncated) {
-  ldpp_dout(dpp, 10) << __func__ << ": TODO" << dendl;
+  *truncated = false;
+  rgw::sal::sfs::sqlite::SQLiteUsers sqlite_users(dpp->get_cct());
+  auto ids = sqlite_users.getUserIDs();
+  std::copy(ids.begin(), ids.end(), std::back_inserter(keys));
   return 0;
 }
 

0xavi0/ceph

src/rgw/rgw_sal_sfs.cc
@@ -27,6 +27,7 @@
 #include "driver/sfs/bucket_mgr.h"
 #include "driver/sfs/notification.h"
 #include "driver/sfs/writer.h"
+#include "rgw/driver/sfs/sqlite/sqlite_users.h"
 #include "rgw_acl_s3.h"
 #include "rgw_aio.h"
 #include "rgw_aio_throttle.h"
@@ -130,8 +131,7 @@ int SFStore::forward_request_to_master(
     const DoutPrefixProvider* dpp, User* user, obj_version* objv,
     bufferlist& in_data, JSONParser* jp, req_info& info, optional_yield y
 ) {
-  ldpp_dout(dpp, 10) << __func__ << ": TODO" << dendl;
-  return -ENOTSUP;
+  return 0;
 }
 
 int SFStore::forward_iam_request_to_master(
@@ -253,7 +253,10 @@ int SFStore::meta_list_keys_next(
     const DoutPrefixProvider* dpp, void* handle, int max, list<string>& keys,
     bool* truncated
 ) {
-  ldpp_dout(dpp, 10) << __func__ << ": TODO" << dendl;
+  *truncated = false;
+  rgw::sal::sfs::sqlite::SQLiteUsers sqlite_users(dpp->get_cct());
+  auto ids = sqlite_users.getUserIDs();
+  std::copy(ids.begin(), ids.end(), std::back_inserter(keys));
   return 0;
 }
 